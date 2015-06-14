115a469e73d7e0ccc853aee1f01d498e5605dbd1
Reaz Hasan <rhasan@gmail.com>
2015-06-14 07:21:07 -0500

    Filled in for completeness

--- p3/m1.c
+++ p3/m1.c
@@ -3,19 +3,28 @@
 # include <stdio.h>
 
 int main(void) {
-	int target;
+	long target;
 	int factor;
 	int largestFactor;
 	int square;
-	target=600851475143;
+	target=600851475143L;
+	long orig_target;
+	orig_target = target;
 	factor = 2;
 	largestFactor = 0;
-	while ((factor * factor) < target) {
-		
-		
+	while (target > 1) {
+          if (!(target % factor)) {
+	     /* divisible by factor */		
+	     largestFactor = factor; 
+             target = target / factor; 
+             while (!(target % factor)) {
+		target = target / factor;
+	     }
+	  }
+	  factor += 1;		
 	};
 	
-	printf("Square: %d\n", square); 
+	printf("Largest Factor: %d\n", largestFactor); 
 
 	return 0;
 }
